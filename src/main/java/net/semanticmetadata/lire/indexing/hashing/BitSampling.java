ca71f106f3690c6839dfe8cdbcb4a42414a0b81b
Erich Duda <dudaerich@gmail.com>
2015-02-13 08:12:20 +0100

    Repaired maven settings.

--- src/main/java/net/semanticmetadata/lire/indexing/hashing/BitSampling.java
+++ src/main/java/net/semanticmetadata/lire/indexing/hashing/BitSampling.java
@@ -66,7 +66,7 @@ public class BitSampling {
 
 
     // Dimensions should cover the maximum dimensions of descriptors used with bit sampling
-    public static int dimensions = 640;
+    public static int dimensions = 3100;
 
     public static final String hashFunctionsFileName = "LshBitSampling.obj";
     private static double[][][] hashes = null;
@@ -145,7 +145,7 @@ public class BitSampling {
      * @throws IOException
      */
     public static double[][][] readHashFunctions() throws IOException {
-        ObjectInputStream ois = new ObjectInputStream(new GZIPInputStream(BitSampling.class.getResourceAsStream(hashFunctionsFileName)));
+        ObjectInputStream ois = new ObjectInputStream(new GZIPInputStream(BitSampling.class.getResourceAsStream("/" + hashFunctionsFileName)));
         int bits = ois.readInt();
         int dimensions = ois.readInt();
         int numFunctionBundles = ois.readInt();
